af2ac2dfc67044d9329e5307978461b50fc49b8a
Edilflow User <user@edilflow.local>
2026-03-17 17:48:28 +0100

    Final: Added cash ingestion fix and provided Excel sample.

--- _list_sheets.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import os
-
-path = r"C:\Users\Utente\Desktop\Lavoro\Lavoro_Giupponi6\data\db_schema\INPUT\CONTANTI\movimenti doc finance 02.03_09.03.xls"
-if os.path.exists(path):
-    xls = pd.ExcelFile(path)
-    print("Sheets:", xls.sheet_names)
-else:
-    print("File not found")
